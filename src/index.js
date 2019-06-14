4d3abc829bb2fba1c4de8e35b316f4abd5bc6013
gnarmedia <gnarmedia@users.noreply.github.com>
2019-06-14 02:11:06 -0700

    Begin some UI theming

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "semantic-ui-css/semantic.min.css";
-import "./assets/style.css";
+import "milligram";
 
 import App from "./components/App";
 
