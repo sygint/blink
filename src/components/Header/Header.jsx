762d9bd2cbe6886aef04b5bedc3ed091bb1e2409
gnarmedia <gnarmedia@users.noreply.github.com>
2019-06-19 03:43:13 -0700

    Add logout icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,19 @@
 import React from "react";
 
 import { ReactComponent as Logo } from "../../assets/images/agenda.svg";
+import { ReactComponent as LogOut } from "../../assets/images/logout.svg";
 
 export default function Header({ isSignedIn, handleSignIn, handleSignOut }) {
   return (
     <nav className="navigation">
       <a className="navigation-title-link" href="/">
         <Logo className="logo header" />
+        <h1 className="logo-title">Bookmarks</h1>
       </a>
 
       <ul className="navigation-list">
         <li className="navigation-item">
-          {!isSignedIn ? (
-            <button className="button header" onClick={handleSignIn}>
-              Sign in with Bockstack
-            </button>
-          ) : (
-            <button className="button header" onClick={handleSignOut}>
-              Sign out
-            </button>
-          )}
+          <LogOut className="logout header" onClick={handleSignOut} />
         </li>
       </ul>
     </nav>
