5a1940f33e18009308e3c094e6c9fcac998dc034
gnarmedia <gnarmedia@users.noreply.github.com>
2019-06-19 02:37:45 -0700

    Fix Header.jsx formatting

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,24 +4,24 @@ import { ReactComponent as Logo } from "../../assets/images/agenda.svg";
 
 export default function Header({ isSignedIn, handleSignIn, handleSignOut }) {
   return (
-      <nav className="navigation">
-        <a className="navigation-title-link" href="/">
-          <Logo className="logo header" />
-        </a>
+    <nav className="navigation">
+      <a className="navigation-title-link" href="/">
+        <Logo className="logo header" />
+      </a>
 
-        <ul className="navigation-list">
-          <li className="navigation-item">
-            {!isSignedIn ? (
-              <button className="button header" onClick={handleSignIn}>
-                Sign in with Bockstack
-              </button>
-            ) : (
-              <button className="button header" onClick={handleSignOut}>
-                Sign out
-              </button>
-            )}
-          </li>
-        </ul>
-      </nav>
+      <ul className="navigation-list">
+        <li className="navigation-item">
+          {!isSignedIn ? (
+            <button className="button header" onClick={handleSignIn}>
+              Sign in with Bockstack
+            </button>
+          ) : (
+            <button className="button header" onClick={handleSignOut}>
+              Sign out
+            </button>
+          )}
+        </li>
+      </ul>
+    </nav>
   );
 }
