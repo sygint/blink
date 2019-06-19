c1419636bfcd6114e91e2518dddd7c5d322b3f2f
gnarmedia <gnarmedia@users.noreply.github.com>
2019-06-19 00:00:32 -0700

    Remove 3rd party style dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "milligram";
-import "./assets/style.scss";
-
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
