6f8d7318083da93768c7c867906628b67250319f
gnarmedia <gnarmedia@users.noreply.github.com>
2019-06-29 00:36:49 -0700

    Fix form submission and revise classNames

--- src/components/BookmarkForm/BookmarkForm.jsx
+++ src/components/BookmarkForm/BookmarkForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Field, Form } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object().shape({
@@ -10,43 +10,35 @@ const validationSchema = Yup.object().shape({
 export default function BookmarkForm({ onSubmit }) {
   return (
     <Formik
-      initialValues={{
-        title: "",
-        url: ""
-      }}
+      initialValues={{ title: "", url: "" }}
       validationSchema={validationSchema}
-      onSubmit={(values, actions) => {
+      onSubmit={(values, { setSubmitting }) => {
         onSubmit(values);
+        setSubmitting(false);
       }}
-      render={({ errors, status, touched, isSubmitting }) => (
-        <Form className="form bookmark-form">
-          <div className="form-field">
-            <label htmlFor="title" className="form-label">
-              Title:
-            </label>
-            <Field className="form-input" type="text" name="title" />
-            {errors.title && touched.title && (
-              <div className="form-error">{errors.title}</div>
-            )}
-          </div>
-
-          <div className="form-field">
-            <label htmlFor="url" className="form-label">
-              Url:
-            </label>
-            <Field className="form-input" type="text" name="url" />
-            {errors.url && touched.url && (
-              <div className="form-error">{errors.url}</div>
-            )}
-          </div>
-
-          {status && status.msg && <div>{status.msg}</div>}
-
-          <button
-            className="button form-submit"
-            type="submit"
-            disabled={isSubmitting}
-          >
+      render={({ handleSubmit, isSubmitting }) => (
+        <Form onSubmit={handleSubmit} className="form bookmark-form">
+          <label htmlFor="title" className="bookmark-form_label">
+            Title:
+          </label>
+          <Field
+            className="form-input"
+            type="text"
+            name="title"
+            className="bookmark-form_field"
+          />
+          <ErrorMessage name="title" component="div" className="form-error" />
+          <label htmlFor="url" className="bookmark-form_label">
+            Url:
+          </label>
+          <Field
+            className="form-input"
+            type="text"
+            name="url"
+            className="bookmark-form_field"
+          />
+          <ErrorMessage name="url" component="div" className="form-error" />
+          <button type="submit" disabled={isSubmitting}>
             Submit
           </button>
         </Form>
