8e8bc7b9a5a35cd70ed74d3da6f4c3f24d8e0929
gnarmedia <gnarmedia@users.noreply.github.com>
2019-07-25 15:53:40 -0700

    Begin styling of bookmark list

--- src/components/BookmarksList/BookmarksList.jsx
+++ src/components/BookmarksList/BookmarksList.jsx
@@ -2,15 +2,28 @@ import React from "react";
 
 export default function BookmarksList({ bookmarks, onDeleteBookmark }) {
   return (
-    <ul>
-      {bookmarks.map(({ title, url, id }) => (
-        <li key={id}>
+    <ul className="bookmark-list">
+      {bookmarks.map(({ title, url, id, lead_image_url, excerpt, domain }) => {
+        const scheme = url.substr(0, url.indexOf('://') + 3);
+
+        return (
+        <li key={id} className="bookmark-list_item">
           <>
-            <a href={url}>{title}</a>{" "}
+              <a href={url} className="bookmark_img-container">
+                <img src={lead_image_url} className="bookmark_img" alt={title} />
+              </a>
+              <a href={url} className="bookmark_title">
+                {title}
+            </a>
+              <a href={scheme + domain}>{domain}</a>
+              <p>
+                {excerpt}
+              </p>
             <button onClick={() => onDeleteBookmark(id)}>delete</button>
           </>
         </li>
-      ))}
+        );
+      })}
     </ul>
   );
 }
