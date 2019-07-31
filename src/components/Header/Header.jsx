1e55d657f973033680cc4bf11988e871a160a777
gnarmedia <gnarmedia@users.noreply.github.com>
2019-07-31 02:20:32 -0700

    Redo header icons and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 import { ReactComponent as Logo } from "../../assets/images/agenda.svg";
-import { ReactComponent as LogOut } from "../../assets/images/logout.svg";
+import { ReactComponent as LogOut } from "../../assets/images/log-out.svg";
 
 export default function Header({ handleSignOut }) {
   return (
     <nav className="navigation">
       <a className="navigation-title-link" href="/">
-        <Logo className="logo header" />
+        <Logo className="logo header-icon" />
         <h1 className="logo-title">Blink</h1>
       </a>
 
       <ul className="navigation-list">
         <li className="navigation-item">
-          <LogOut className="logout header" onClick={handleSignOut} />
+          <LogOut className="logout header-icon" onClick={handleSignOut} />
         </li>
       </ul>
     </nav>
