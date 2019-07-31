96878794b70f4065aab2e3c360f65d5d0dee732d
gnarmedia <gnarmedia@users.noreply.github.com>
2019-07-30 22:12:24 -0700

    Style extras and space title

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -10,8 +10,12 @@ export default function Bookmark({ onDeleteBookmark, bookmark }) {
       <a href={url} className="bookmark_title">
         {title}
       </a>
-      <a href={scheme + domain}>{domain}</a>
-      <p>{excerpt}</p>
+      <div className="bookmark_extras">
+        <a href={scheme + domain} className="bookmark_domain">
+          {domain}
+        </a>
+      </div>
+      <p className="bookmark_excerpt">{excerpt}</p>
       <button onClick={() => onDeleteBookmark(id)}>delete</button>
     </>
   );
