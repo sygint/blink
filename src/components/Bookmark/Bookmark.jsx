1f523af74132b68a17417417e3b32fd672a3938b
gnarmedia <gnarmedia@users.noreply.github.com>
2019-08-10 22:52:05 -0700

    Handle malformed bookmarks

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 
 export default function Bookmark({ onDeleteBookmark, bookmark }) {
-  const { title, url, id, thumbnail, excerpt, domain } = bookmark;
-  const scheme = url.substr(0, url.indexOf("://") + 3);
+  try {
+    const { title, url, id, thumbnail, excerpt, domain } = bookmark;
+    const scheme = url.substr(0, url.indexOf("://") + 3);
 
-  return (
-    <>
-      <a href={url} className="bookmark_title">
-        <div
-          className="bookmark_thumbnail"
-          style={{ backgroundImage: `url(${thumbnail}` }}
-        />
-        {title}
-      </a>
-      <div className="bookmark_extras">
-        <a href={scheme + domain} className="bookmark_domain">
-          {domain}
+    return (
+      <>
+        <a href={url} className="bookmark_title">
+          <div
+            className="bookmark_thumbnail"
+            style={{ backgroundImage: `url(${thumbnail}` }}
+          />
+          {title}
         </a>
-      </div>
-      <p className="bookmark_excerpt">{excerpt}</p>
-      <button onClick={() => onDeleteBookmark(id)}>delete</button>
-    </>
-  );
+        <div className="bookmark_extras">
+          <a href={scheme + domain} className="bookmark_domain">
+            {domain}
+          </a>
+        </div>
+        <p className="bookmark_excerpt">{excerpt}</p>
+        <button onClick={() => onDeleteBookmark(id)}>delete</button>
+      </>
+    );
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
 }
