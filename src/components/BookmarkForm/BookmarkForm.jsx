2f32f7fe7d0957619374415e8e0a419178635429
gnarmedia <gnarmedia@users.noreply.github.com>
2019-08-13 00:45:22 -0700

    feat(bookmarkform): add placeholder text for easier usage

--- src/components/BookmarkForm/BookmarkForm.jsx
+++ src/components/BookmarkForm/BookmarkForm.jsx
@@ -19,6 +19,7 @@ export default function BookmarkForm({ onSubmit, handleHideAddBookmarks }) {
             onSubmit(data);
           }
         }}
+        placeHolder="Add a website https://..."
       />
       <button
         type="submit"
