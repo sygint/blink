2ac67cba794980dbadee67319bf0b29ae8573277
gnarmedia <gnarmedia@users.noreply.github.com>
2019-08-18 20:47:04 -0700

    refactor(main): add PropTypes

--- src/components/04-Ecosystems/Main/Main.jsx
+++ src/components/04-Ecosystems/Main/Main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const Container = styled.main`
   flex-grow: 1;
 `;
 
-export default function Main({ children }) {
+function Main({ children }) {
   return <Container>{children}</Container>;
 }
+
+Main.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Main;
