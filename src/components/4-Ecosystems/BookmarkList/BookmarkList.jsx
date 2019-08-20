4e9efeca4196caa4f95a3a2c5126af23a6c04ae1
gnarmedia <gnarmedia@users.noreply.github.com>
2019-08-19 22:57:26 -0700

    refactor(bookmarklist): convert to styled components

--- src/components/4-Ecosystems/BookmarkList/BookmarkList.jsx
+++ src/components/4-Ecosystems/BookmarkList/BookmarkList.jsx
@@ -1,19 +1,45 @@
 import React from "react";
+import styled from "styled-components";
 import PropTypes from "prop-types";
 
 import Bookmark from "../../3-Organisms/Bookmark";
 
+const StyledBookmarkList = styled.ul`
+  list-style: none;
+  padding-left: 0;
+
+  @media ${({ theme: { breakpoint } }) => breakpoint.mobileLandscape} {
+    display: grid;
+    grid-column-gap: 30px;
+    grid-row-gap: 20px;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+  }
+
+  @media ${({ theme: { breakpoint } }) => breakpoint.tabletPortrait} {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+`;
+
+const BookmarkListItem = styled.li`
+  border-bottom: 1px solid ${({ theme: { color } }) => color.third};
+  height: 460px;
+  margin: 0 auto 32px;
+  padding-bottom: 16px;
+  position: relative;
+`;
+
 function BookmarkList({ bookmarks, onDeleteBookmark }) {
   return (
-    <ul className="bookmark-list">
+    <StyledBookmarkList>
       {bookmarks.map(bookmark => {
         return (
-          <li key={bookmark.id} className="bookmark-list_item">
+          <BookmarkListItem key={bookmark.id}>
             <Bookmark onDeleteBookmark={onDeleteBookmark} bookmark={bookmark} />
-          </li>
+          </BookmarkListItem>
         );
       })}
-    </ul>
+    </StyledBookmarkList>
   );
 }
 
