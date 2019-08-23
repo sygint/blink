e871834f866cee5a94a9c65f88933076a1b54ec7
gnarmedia <gnarmedia@users.noreply.github.com>
2019-08-23 16:51:40 -0700

    refactor(bookmark, bookmarks.scss): convert Bookmarks to styled component

--- src/components/3-Organisms/Bookmark/Bookmark.jsx
+++ src/components/3-Organisms/Bookmark/Bookmark.jsx
@@ -1,7 +1,70 @@
 import React from "react";
+import styled from "styled-components";
 import PropTypes from "prop-types";
 
 import Icon from "../../1-Atoms/Icon";
+
+const Thumbnail = styled.a`
+  background: ${({ image }) => `url(${image})`};
+  background-position: center center;
+  background-size: cover;
+  border-radius: 4px;
+  display: block;
+  height: 250px;
+  margin-bottom: 16px;
+  text-indent: -999999px;
+  width: 100%;
+`;
+
+const Title = styled.a`
+  -webkit-box-orient: vertical; /* stylelint-disable-line property-no-vendor-prefix */
+  color: #000;
+  display: -webkit-box; /* stylelint-disable-line value-no-vendor-prefix */
+  font-weight: 600;
+  -webkit-line-clamp: 3;
+  max-height: 4.4em;
+  overflow: hidden;
+  text-align: justify;
+  text-decoration: none;
+`;
+
+const Extras = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 8px 0;
+`;
+
+const Domain = styled.a`
+  color: #000;
+  font-weight: 100;
+  text-decoration: none;
+`;
+
+const ReadingTime = styled.span`
+  align-items: center;
+  display: flex;
+`;
+
+const ReadingTimeIcon = styled(Icon)`
+  margin-right: 5px;
+`;
+
+const Excerpt = styled.p`
+  margin-top: 0;
+  max-height: 3em;
+  overflow: hidden;
+  text-align: justify;
+`;
+
+const Actions = styled.div`
+  bottom: 10px;
+  position: absolute;
+  right: 0;
+`;
+
+const Delete = styled(Icon)`
+  cursor: pointer;
+`;
 
 function Bookmark({ onClickDelete, bookmark }) {
   try {
@@ -10,37 +73,21 @@ function Bookmark({ onClickDelete, bookmark }) {
 
     return (
       <>
-        <a
-          href={url}
-          className="bookmark_thumbnail"
-          style={{ backgroundImage: `url(${thumbnail}` }}
-        >
+        <Thumbnail href={url} image={thumbnail}>
           {title}
-        </a>
-        <a href={url} className="bookmark_title">
-          {title}
-        </a>
-        <div className="bookmark_extras">
-          <a href={scheme + domain} className="bookmark_domain">
-            {domain}
-          </a>
-          <span className="bookmark_reading-time">
-            <Icon
-              name="Clock"
-              size={16}
-              className="bookmark_reading-time_icon"
-            />
+        </Thumbnail>
+        <Title href={url}>{title}</Title>
+        <Extras>
+          <Domain href={scheme + domain}>{domain}</Domain>
+          <ReadingTime>
+            <ReadingTimeIcon name="Clock" size={16} />
             {Math.round(wordCount / 225)} min
-          </span>
-        </div>
-        <p className="bookmark_excerpt">{excerpt}</p>
-        <div className="bookmark_actions">
-          <Icon
-            name="Trash2"
-            onClick={() => onClickDelete(id)}
-            className="bookmark_icon"
-          />
-        </div>
+          </ReadingTime>
+        </Extras>
+        <Excerpt>{excerpt}</Excerpt>
+        <Actions>
+          <Delete name="Trash2" onClick={() => onClickDelete(id)} />
+        </Actions>
       </>
     );
   } catch (e) {
