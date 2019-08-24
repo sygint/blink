33644c3c278cb975639fb61a9b6dd8805d267d4b
gnarmedia <gnarmedia@users.noreply.github.com>
2019-08-23 17:55:58 -0700

    refactor(bookmark): convert to styled component

--- src/components/3-Organisms/BookmarkForm/BookmarkForm.jsx
+++ src/components/3-Organisms/BookmarkForm/BookmarkForm.jsx
@@ -1,18 +1,62 @@
 import React from "react";
+import styled from "styled-components";
 import PropTypes from "prop-types";
 
 import { X as Close } from "react-feather";
 
+const StyledForm = styled.form`
+  background: #eee;
+  border-radius: 4px;
+  box-sizing: border-box;
+  display: flex;
+  height: $head-height - $spacing * 2;
+  left: 20px;
+  padding: 5px 5px 5px 15px;
+  position: absolute;
+  right: 59px;
+  top: 10px;
+  z-index: 510;
+`;
+
+const StyledLabel = styled.label`
+  display: none;
+  font-size: 1rem;
+  font-weight: 600;
+  line-height: normal;
+  margin-right: 10px;
+
+  ::after {
+    content: ":";
+  }
+`;
+
+const StyledInput = styled.input`
+  background: none;
+  border: 0;
+  box-sizing: border-box;
+  color: darken($header-icon-color, 25);
+  flex-grow: 1;
+  font-size: 18px;
+  height: 100%;
+
+  ::placeholder {
+    color: lighten($header-icon-color, 15);
+  }
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: 0;
+  padding: 0;
+`;
+
 function BookmarkForm({ onSubmit, handleHideAddBookmarks }) {
   return (
-    <form className="form bookmark-form">
-      <label htmlFor="url" className="bookmark-form_label">
-        Url
-      </label>
-      <input
+    <StyledForm>
+      <StyledLabel htmlFor="url">Url</StyledLabel>
+      <StyledInput
         type="text"
         name="url"
-        className="bookmark-form_field"
         onKeyDown={event => {
           const data = new FormData(event.target.closest("form"));
 
@@ -24,14 +68,10 @@ function BookmarkForm({ onSubmit, handleHideAddBookmarks }) {
         }}
         placeholder="Add a website https://..."
       />
-      <button
-        type="submit"
-        className="bookmark-form_button"
-        onClick={handleHideAddBookmarks}
-      >
+      <StyledButton type="submit" onClick={handleHideAddBookmarks}>
         <Close color="#888" />
-      </button>
-    </form>
+      </StyledButton>
+    </StyledForm>
   );
 }
 
