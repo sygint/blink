a316bb8d1e78c3772dfa26a3541e231753107ab3
gnarmedia <gnarmedia@users.noreply.github.com>
2019-08-26 12:26:02 -0700

    refactor(readingtime): abstract styled components into styles and simplify naming

--- src/components/2-Molecules/ReadingTime/ReadingTime.jsx
+++ src/components/2-Molecules/ReadingTime/ReadingTime.jsx
@@ -1,24 +1,14 @@
 import React from "react";
-import styled from "styled-components";
 import PropTypes from "prop-types";
 
-import Icon from "../../1-Atoms/Icon";
-
-const StyledReadingTime = styled.span`
-  align-items: center;
-  display: flex;
-`;
-
-const StyledIcon = styled(Icon)`
-  margin-right: 5px;
-`;
+import { Span, StyledIcon } from "./styles";
 
 function ReadingTime({ wordCount }) {
   return (
-    <StyledReadingTime>
+    <Span>
       <StyledIcon name="Clock" size={16} />
       {Math.round(wordCount / 225)} min
-    </StyledReadingTime>
+    </Span>
   );
 }
 
---
+++ src/components/2-Molecules/ReadingTime/styles.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/prefer-default-export */
+
+import styled from "styled-components";
+
+import Icon from "../../1-Atoms/Icon";
+
+export const Span = styled.span`
+  align-items: center;
+  display: flex;
+`;
+
+export const StyledIcon = styled(Icon)`
+  margin-right: 5px;
+`;
