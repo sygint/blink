5df1513c874af0950bce8c32194ec1597ab01719
gnarmedia <gnarmedia@users.noreply.github.com>
2019-08-26 12:33:48 -0700

    refactor(bookmark): abstract styled components into styles

--- src/components/3-Organisms/Bookmark/Bookmark.jsx
+++ src/components/3-Organisms/Bookmark/Bookmark.jsx
@@ -1,57 +1,9 @@
 import React from "react";
-import styled from "styled-components";
 import PropTypes from "prop-types";
 
-import Icon from "../../1-Atoms/Icon";
+import { Thumbnail, Title, Extras, Excerpt, Actions, Delete } from "./styles";
 import Domain from "../../2-Molecules/Domain";
 import ReadingTime from "../../2-Molecules/ReadingTime";
-
-const Thumbnail = styled.a`
-  background: ${({ image }) => `url(${image})`};
-  background-position: center center;
-  background-size: cover;
-  border-radius: 4px;
-  display: block;
-  height: 250px;
-  margin-bottom: 16px;
-  text-indent: -999999px;
-  width: 100%;
-`;
-
-const Title = styled.a`
-  -webkit-box-orient: vertical; /* stylelint-disable-line property-no-vendor-prefix */
-  color: #000;
-  display: -webkit-box; /* stylelint-disable-line value-no-vendor-prefix */
-  font-weight: 600;
-  -webkit-line-clamp: 3;
-  max-height: 4.4em;
-  overflow: hidden;
-  text-align: justify;
-  text-decoration: none;
-`;
-
-const Extras = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin: 8px 0;
-`;
-
-const Excerpt = styled.p`
-  margin-top: 0;
-  max-height: 3em;
-  overflow: hidden;
-  text-align: justify;
-`;
-
-const Actions = styled.div`
-  bottom: 10px;
-  position: absolute;
-  right: 0;
-`;
-
-const Delete = styled(Icon)`
-  cursor: pointer;
-`;
 
 function Bookmark({ onClickDelete, bookmark }) {
   try {
---
+++ src/components/3-Organisms/Bookmark/styles.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+import Icon from "../../1-Atoms/Icon";
+
+export const Thumbnail = styled.a`
+  background: ${({ image }) => `url(${image})`};
+  background-position: center center;
+  background-size: cover;
+  border-radius: 4px;
+  display: block;
+  height: 250px;
+  margin-bottom: 16px;
+  text-indent: -999999px;
+  width: 100%;
+`;
+
+export const Title = styled.a`
+  -webkit-box-orient: vertical; /* stylelint-disable-line property-no-vendor-prefix */
+  color: #000;
+  display: -webkit-box; /* stylelint-disable-line value-no-vendor-prefix */
+  font-weight: 600;
+  -webkit-line-clamp: 3;
+  max-height: 4.4em;
+  overflow: hidden;
+  text-align: justify;
+  text-decoration: none;
+`;
+
+export const Extras = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 8px 0;
+`;
+
+export const Excerpt = styled.p`
+  margin-top: 0;
+  max-height: 3em;
+  overflow: hidden;
+  text-align: justify;
+`;
+
+export const Actions = styled.div`
+  bottom: 10px;
+  position: absolute;
+  right: 0;
+`;
+
+export const Delete = styled(Icon)`
+  cursor: pointer;
+`;
