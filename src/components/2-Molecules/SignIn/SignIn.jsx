5ff3ab64cd34f6589c258cf65e86ff4435b03a76
gnarmedia <gnarmedia@users.noreply.github.com>
2019-08-26 12:28:56 -0700

    refactor(signin): abstract styled components into styles and simplify naming

--- src/components/2-Molecules/SignIn/SignIn.jsx
+++ src/components/2-Molecules/SignIn/SignIn.jsx
@@ -1,54 +1,17 @@
 import React from "react";
-import styled from "styled-components";
 import PropTypes from "prop-types";
 
-import { ReactComponent as Logo } from "../../../assets/images/agenda.svg";
-
-const Container = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
-  justify-content: center;
-`;
-
-const StyledLogo = styled(Logo)`
-  height: auto;
-  width: 50%;
-`;
-
-const Title = styled.h1`
-  color: $brand-color;
-  margin: 0;
-`;
-
-const Button = styled.button`
-  background-color: ${({ theme: { color } }) => color.button};
-  border: 0.1rem solid ${({ theme: { color } }) => color.buttonBorder};
-  border-radius: 0.4rem;
-  color: #fff;
-  cursor: pointer;
-  display: inline-block;
-  font-family: fira-sans-2, sans-serif;
-  font-size: 1.1rem;
-  font-weight: 600;
-  letter-spacing: 0.1rem;
-  padding: 5px;
-  text-align: center;
-  text-decoration: none;
-  text-transform: uppercase;
-  white-space: nowrap;
-`;
+import { StyledDiv, StyledLogo, Title, Button } from "./styles";
 
 function SignIn({ onClickSignIn }) {
   return (
-    <Container>
+    <StyledDiv>
       <StyledLogo />
       <Title>Blink</Title>
       <Button type="button" onClick={onClickSignIn}>
         Sign in with Bockstack
       </Button>
-    </Container>
+    </StyledDiv>
   );
 }
 
---
+++ src/components/2-Molecules/SignIn/styles.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+import { ReactComponent as Logo } from "../../../assets/images/agenda.svg";
+
+export const StyledDiv = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  justify-content: center;
+`;
+
+export const StyledLogo = styled(Logo)`
+  height: auto;
+  width: 50%;
+`;
+
+export const Title = styled.h1`
+  color: $brand-color;
+  margin: 0;
+`;
+
+export const Button = styled.button`
+  background-color: ${({ theme: { color } }) => color.button};
+  border: 0.1rem solid ${({ theme: { color } }) => color.buttonBorder};
+  border-radius: 0.4rem;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-family: fira-sans-2, sans-serif;
+  font-size: 1.1rem;
+  font-weight: 600;
+  letter-spacing: 0.1rem;
+  padding: 5px;
+  text-align: center;
+  text-decoration: none;
+  text-transform: uppercase;
+  white-space: nowrap;
+`;
