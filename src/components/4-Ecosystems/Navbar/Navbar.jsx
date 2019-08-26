0ceb882fd472ae011cdc793fd4621ff015eadc59
gnarmedia <gnarmedia@users.noreply.github.com>
2019-08-26 13:06:56 -0700

    refactor(navbar): abstract styled components into styles

--- src/components/4-Ecosystems/Navbar/Navbar.jsx
+++ src/components/4-Ecosystems/Navbar/Navbar.jsx
@@ -1,33 +1,9 @@
 import React from "react";
-import styled from "styled-components";
 import PropTypes from "prop-types";
 
+import { StyledNav, NavbarLeft, NavbarRight } from "./styles";
 import Branding from "../../2-Molecules/Branding";
 import IconButton from "../../2-Molecules/IconButton";
-
-const StyledNav = styled.nav`
-  align-items: center;
-  background: #fff;
-  border-bottom: 1px solid ${({ theme: { color } }) => color.third};
-  box-sizing: border-box;
-  display: flex;
-  height: ${({ theme: { dimension } }) => dimension.mastheadHeight};
-  justify-content: space-between;
-  padding: 0 20px;
-`;
-
-const NavbarLeft = styled.div``;
-const NavbarRight = styled.div`
-  display: flex;
-
-  > button:last-child::before {
-    border-left: 1px solid ${({ theme: { color } }) => color.icon};
-    content: "";
-    height: 35px;
-    margin-right: 10px;
-    margin-left: 5px;
-  }
-`;
 
 function Navbar({ onClickSignOut, onClickAdd }) {
   return (
---
+++ src/components/4-Ecosystems/Navbar/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const StyledNav = styled.nav`
+  align-items: center;
+  background: #fff;
+  border-bottom: 1px solid ${({ theme: { color } }) => color.third};
+  box-sizing: border-box;
+  display: flex;
+  height: ${({ theme: { dimension } }) => dimension.mastheadHeight};
+  justify-content: space-between;
+  padding: 0 20px;
+`;
+
+export const NavbarLeft = styled.div``;
+export const NavbarRight = styled.div`
+  display: flex;
+
+  > button:last-child::before {
+    border-left: 1px solid ${({ theme: { color } }) => color.icon};
+    content: "";
+    height: 35px;
+    margin-left: 5px;
+    margin-right: 10px;
+  }
+`;
