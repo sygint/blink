4c0d2da3131c7348ebd748e71bdd698e46119e86
gnarmedia <gnarmedia@users.noreply.github.com>
2019-09-11 03:00:32 -0700

    refactor(bookmarks): abstract bookmarks logic into API module

--- src/index.js
+++ src/index.js
@@ -3,26 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 
-import bookmarksHelper from "./api/bookmarkHelpers";
-import mockBookmarksHelper from "./__mocks__/bookmarkHelpers";
-
+import bookmarks from "./api/bookmarks";
 import blockstack from "./api/blockstack";
 import theme from "./theme";
 import App from "./components/5-Environments/App";
 
-let bookmarkApi;
-
 const offlineMode = process.env.REACT_APP_OFFLINE === "true";
 const { userSession } = blockstack(offlineMode);
+const bookmarkApi = bookmarks(userSession, offlineMode);
 
 window.userSession = userSession;
-
-if (offlineMode) {
-  console.log("*** using offline mode ***");
-  bookmarkApi = mockBookmarksHelper();
-} else {
-  bookmarkApi = bookmarksHelper(userSession);
-}
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
