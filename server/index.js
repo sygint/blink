2d517f4d6d26efc7068360084ab9868700c7f3e6
gnarmedia <gnarmedia@users.noreply.github.com>
2019-10-05 04:00:22 -0700

    refactor(server): break fetch out of Mercury

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const asyncHandler = require('express-async-handler');
+const asyncHandler = require("express-async-handler");
 const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
 const Mercury = require("@postlight/mercury-parser");
 
 const app = express();
@@ -8,16 +9,22 @@ const port = 4000;
 
 app.use(bodyParser.json());
 
-app.post("/extract", asyncHandler(async (req, res) => {
-  try {
-    const bookmarkData = await Mercury.parse(req.body.url);
-    
-    res.send(bookmarkData);
-  }
-  catch(e) {
-    console.trace(e);
-  }
-}));
+app.post(
+  "/extract",
+  asyncHandler(async (req, res) => {
+    const { url } = req.body;
+
+    try {
+      const response = await fetch(url);
+      const html = await response.text();
+      const bookmarkData = await Mercury.parse(url, { html });
+
+      res.send(bookmarkData);
+    } catch (e) {
+      console.trace(e);
+    }
+  })
+);
 
 app.listen(port, err => {
   if (err) throw err;
