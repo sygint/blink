e378bd7d428d5d70e9f3da091f015dc8f323f8eb
gnarmedia <gnarmedia@users.noreply.github.com>
2019-12-05 01:28:23 -0800

    refactor(bookmarkhelpers): improve getBookmarks test by removing implementation details

--- src/api/bookmarks/bookmarkHelpers.test.js
+++ src/api/bookmarks/bookmarkHelpers.test.js
@@ -9,6 +9,23 @@ const mockBookmark = {
   wordCount: 25,
   id: "1234567890"
 };
+const mockBookmark1 = mockBookmark;
+const mockBookmark2 = {
+  url: "http://example.net",
+  excerpt: "Example excerpt 2",
+  title: "mock title 2",
+  thumbnail: "mock thumbnail 2",
+  wordCount: 30,
+  id: "mock2"
+};
+const mockBookmark3 = {
+  url: "http://example.org",
+  excerpt: "Example excerpt 3",
+  title: "mock title 3",
+  thumbnail: "mock thumbnail 3",
+  wordCount: 35,
+  id: "mock3"
+};
 const mockArticle = {
   author: "Mock Author",
   content: "mock content",
@@ -18,57 +35,27 @@ const mockArticle = {
   domain: "example.com",
   id: "1234567890"
 };
+const mockBookmarks = [mockBookmark1, mockBookmark2, mockBookmark3];
+const mockBookmarkData = {
+  bookmarkIds: mockBookmarkIds,
+  bookmarks: mockBookmarks
+};
 const mockData = {
   "blink/bookmarkIds.json": mockBookmarkIds,
   "blink/bookmarks/mock.json": mockBookmark,
+  "blink/bookmarks/mock1.json": mockBookmark1,
+  "blink/bookmarks/mock2.json": mockBookmark2,
+  "blink/bookmarks/mock3.json": mockBookmark3,
   "blink/articles/mock.json": mockArticle
 };
 
 const mockBookmarkIdsJson = JSON.stringify(mockBookmarkIds);
 const mockBookmarkJson = JSON.stringify(mockBookmark);
 const mockArticleJson = JSON.stringify(mockArticle);
-const mockBookmarks = [
-  {
-    url: "http://example.com",
-    excerpt: "Example excerpt 1",
-    title: "mock title 1",
-    thumbnail: "mock thumbnail 1",
-    wordCount: 25,
-    id: "mock1"
-  },
-  {
-    url: "http://example.net",
-    excerpt: "Example excerpt 2",
-    title: "mock title 2",
-    thumbnail: "mock thumbnail 2",
-    wordCount: 30,
-    id: "mock2"
-  },
-  {
-    url: "http://example.org",
-    excerpt: "Example excerpt 3",
-    title: "mock title 3",
-    thumbnail: "mock thumbnail 3",
-    wordCount: 35,
-    id: "mock3"
-  }
-];
 
 const mockPutFile = jest.fn();
 const mockGetFile = jest.fn(file => {
-  switch (file) {
-    case "blink/bookmarks/mock1.json":
-      return JSON.stringify(mockBookmarks[0]);
-
-    case "blink/bookmarks/mock2.json":
-      return JSON.stringify(mockBookmarks[1]);
-
-    case "blink/bookmarks/mock3.json":
-      return JSON.stringify(mockBookmarks[2]);
-
-    default:
-      return JSON.stringify(mockData[file]);
-  }
+  return JSON.stringify(mockData[file]);
 });
 
 const mockUserSession = {
@@ -77,7 +64,12 @@ const mockUserSession = {
 };
 
 const bookmarkApi = bookmarkHelpers(mockUserSession);
-const { getBookmarkIds, getBookmark, getArticle } = bookmarkApi;
+const {
+  getBookmarkIds,
+  getBookmark,
+  getArticle,
+  getBookmarks
+} = bookmarkApi;
 
 describe("bookmarkHelpers", () => {
   beforeEach(() => {
@@ -141,13 +133,7 @@ describe("bookmarkHelpers", () => {
 
   describe("getBookmarks()", () => {
     it("should get a list of bookmarks", async () => {
-      mockGetFile.mockReturnValueOnce(mockBookmarkIdsJson);
-      const result = await bookmarkApi.getBookmarks();
-
-      expect(result).toEqual({
-        bookmarkIds: mockBookmarkIds,
-        bookmarks: mockBookmarks
-      });
+      expect(await getBookmarks()).toEqual(mockBookmarkData);
     });
   });
 });
